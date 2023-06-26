4d60685a4ccd5d27f26548c74fb8ee03e4075d88
AlejandraKeber <alejandrakeber@gmail.com>
2023-06-26 15:06:19 -0500

    Add relationship anad validations to models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,9 @@
 class Food < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_foods
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :measurement_unit, presence: true, length: { maximum: 50 }
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :recipes
+  has_many :foods
+
+  validates :name, presence: true, length: { maximum: 50 }
 end
