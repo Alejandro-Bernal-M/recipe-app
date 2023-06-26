8b8cc904a8f556d5bf8068fe54ac0afe00c08b85
alejandro <alejandrober96@gmail.com>
2023-06-26 17:10:21 -0500

    Add recipe index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "users#index"
+  root to: "recipes#index"
+  get '/recipes', to: 'recipes#index'
+  get '/recipes/id', to: 'recipes#show', as: 'recipe'
 end
