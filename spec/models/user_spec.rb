7828d620ecdac32f7e8bcf9caa0ea66ab4b3214b
AlejandraKeber <alejandrakeber@gmail.com>
2023-06-28 12:32:43 -0500

    Add user model test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.create(name: 'User Test', email: 'test@gmail.com', password: '123456', password_confirmation: '123456') }
+
+  before { subject.save }
+
+  context 'Test user validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'name must not be blank' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  end
+end
