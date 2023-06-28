53d56ebee5ad3fe0c5ca05e04ce1761c2ffe8908
alejandro <alejandrober96@gmail.com>
2023-06-28 11:18:36 -0500

    Add view of the public recipes for the unlogged users

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,5 @@
 class PublicRecipesController < ApplicationController
   include PublicRecipesHelper
-  before_action :set_user
   def index
     @recipes = Recipe.includes(:recipe_foods, :user).where(public: true).order(created_at: :desc)
   end
