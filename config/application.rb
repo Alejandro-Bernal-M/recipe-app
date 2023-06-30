3381466dff1714508eaef347577da7959caee0eb
alejandro <alejandrober96@gmail.com>
2023-06-30 10:19:51 -0500

    solve css import error

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module RecipeApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.enabled = true
 
     # Configuration for the application, engines, and railties goes here.
     #
